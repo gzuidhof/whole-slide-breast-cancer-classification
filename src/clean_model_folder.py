48b9d5494c2a530055278b4a19c7599d5ede5c92
Guido Zuidhof <me@guido.io>
2016-09-29 14:00:12 +0200

    Remove unused code, improve dead model remove code

--- src/clean_model_folder.py
+++ src/clean_model_folder.py
@@ -2,15 +2,15 @@ import os
 import glob
 import shutil
 
-folders = sorted(os.listdir('../models'))
+folders = sorted([name for name in os.listdir('../models') if os.path.isdir(os.path.join('../models', name))])
 
 print "Cleaning models without epochs {} folders".format(len(folders))
 
 for model_name in folders:
     folder = '../models/'+model_name
     if len(glob.glob(folder+'/*.npz')) == 0:
-        print "None in ", model_name
+        print "None in ", model_name, ".. removing"
         shutil.rmtree(folder)
 
 folders = sorted(os.listdir('../models'))
-print "Folders remaining: {}".format(len(folders))
+print "Files remaining: {}".format(len(folders))
